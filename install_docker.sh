5b02d803afea1586e911f86b7c0d5bc9e1d3dc50
roysbike <roysbike@gmail.com>
2023-09-27 21:29:06 +0400

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -4,5 +4,5 @@ wget -qO- https://get.docker.com/ | sh
 
 # Install docker-compose
 COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oE "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | sort --version-sort | tail -n 1`
-sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s| sed 's/L/l/'`-`uname -m` > /usr/local/bin/docker-compose"
 sudo chmod +x /usr/local/bin/docker-compose
